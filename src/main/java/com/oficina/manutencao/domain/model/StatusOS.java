7cc0dd46a03a76580f1a0af5e38f3a66f056b3d4
Mateus Lourencao <mateusldias@gmail.com>
2026-08-10 21:12:06 -0300

    feat: criacao das models de manutencao e estoque

---
+++ src/main/java/com/oficina/manutencao/domain/model/StatusOS.java
@@ -0,0 +1,10 @@
+package com.oficina.manutencao.domain.model;
+
+public enum StatusOS {
+    RECEBIDA,
+    EM_DIAGNOSTICO,
+    AGUARDANDO_APROVACAO,
+    EM_EXECUCAO,
+    FINALIZADA,
+    ENTREGUE;
+}
---
+++ src/main/java/com/oficina/manutencao/domain/model/Veiculo.java
@@ -0,0 +1,14 @@
+package com.oficina.manutencao.domain.model;
+
+import java.util.UUID;
+
+public class Veiculo {
+
+    private UUID id;
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int ano;
+
+
+}

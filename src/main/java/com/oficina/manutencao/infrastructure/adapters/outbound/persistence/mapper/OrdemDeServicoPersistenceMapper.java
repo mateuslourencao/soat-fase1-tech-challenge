3cd979228d21373250bb81a63e29632b404424c8
Mateus Lourencao <mateusldias@gmail.com>
2026-08-13 23:08:22 -0300

    feat: separando regras do mapper e do adapter

--- src/main/java/com/oficina/manutencao/infrastructure/adapters/outbound/persistence/mapper/OrdemDeServicoPersistenceMapper.java
+++ src/main/java/com/oficina/manutencao/infrastructure/adapters/outbound/persistence/mapper/OrdemDeServicoPersistenceMapper.java
@@ -1,11 +1,7 @@
 package com.oficina.manutencao.infrastructure.adapters.outbound.persistence.mapper;
 
-import com.oficina.estoque.domain.model.Peca;
-import com.oficina.estoque.domain.model.Servico;
 import com.oficina.estoque.infrastructure.adapters.outbound.persistence.entity.PecaEntity;
 import com.oficina.estoque.infrastructure.adapters.outbound.persistence.entity.ServicoEntity;
-import com.oficina.estoque.infrastructure.adapters.outbound.persistence.repository.PecaJpaRepository;
-import com.oficina.estoque.infrastructure.adapters.outbound.persistence.repository.ServicoJpaRepository;
 import com.oficina.manutencao.domain.model.OrdemDeServico;
 import com.oficina.manutencao.domain.model.PecasNecessarias;
 import com.oficina.manutencao.infrastructure.adapters.outbound.persistence.entity.OrdemDeServicoEntity;
@@ -15,18 +11,12 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class OrdemDeServicoPersistenceMapper {
-    private final PecaJpaRepository pecaRepository;
-    private final ServicoJpaRepository servicoRepository;
 
-    public OrdemDeServicoPersistenceMapper(PecaJpaRepository pecaRepository, ServicoJpaRepository servicoRepository) {
-        this.pecaRepository = pecaRepository;
-        this.servicoRepository = servicoRepository;
-    }
-
-    public OrdemDeServicoEntity toEntity(OrdemDeServico ordem) {
+    public OrdemDeServicoEntity toEntity(OrdemDeServico ordem, Map<Integer, PecaEntity> pecasRef, Map<Integer, ServicoEntity> servicosRef) {
         OrdemDeServicoEntity entity = new OrdemDeServicoEntity();
         entity.setId(ordem.getId());
         entity.setDocumentoCliente(ordem.getDocumentoCliente());
@@ -38,40 +28,40 @@ public class OrdemDeServicoPersistenceMapper {
         entity.setDescricaoQueixas(ordem.getDescricaoQueixas());
         entity.setDiagnosticos(ordem.getDiagnosticos());
         entity.setPecasNecessarias(ordem.getPecasNecessarias().stream()
-                .map(peca -> toPecaEntity(ordem.getId(), peca))
+                .map(peca -> toPecaEntity(ordem.getId(), peca, pecasRef.get(peca.peca().getId())))
                 .toList());
         entity.setServicos(ordem.getServicos().stream()
-                .map(servico -> toServicoEntity(ordem.getId(), servico))
+                .map(servico -> toServicoEntity(ordem.getId(), servico, servicosRef.get(servico.getId())))
                 .toList());
         return entity;
     }
 
     public OrdemDeServico toDomain(OrdemDeServicoEntity entity) {
         List<PecasNecessarias> pecas = entity.getPecasNecessarias().stream().map(this::toPecaDomain).toList();
-        List<Servico> servicos = entity.getServicos().stream().map(this::toServicoDomain).toList();
+        List<com.oficina.estoque.domain.model.Servico> servicos = entity.getServicos().stream().map(this::toServicoDomain).toList();
         return new OrdemDeServico(entity.getId(), entity.getDocumentoCliente(), entity.getPlacaVeiculo(), servicos, pecas,
                 entity.getOrcamento() == null ? 0D : entity.getOrcamento().doubleValue(), entity.getStatus(),
                 entity.getDataCriacao(), entity.getDataAtualizacao(), entity.getDescricaoQueixas(), entity.getDiagnosticos());
     }
 
-    private PecaNecessariaEntity toPecaEntity(int ordemId, PecasNecessarias peca) {
-        return new PecaNecessariaEntity(ordemId, pecaRepository.getReferenceById(peca.peca().getId()),
+    private PecaNecessariaEntity toPecaEntity(int ordemId, PecasNecessarias peca, PecaEntity pecaEntity) {
+        return new PecaNecessariaEntity(ordemId, pecaEntity,
                 peca.quantidade(), BigDecimal.valueOf(peca.getValorUnitario()));
     }
 
     private PecasNecessarias toPecaDomain(PecaNecessariaEntity entity) {
         PecaEntity peca = entity.getPeca();
-        Peca pecaDomain = new Peca(peca.getId(), peca.getDescricao(), entity.getValorUnitario().doubleValue(), peca.getQuantidade());
+        com.oficina.estoque.domain.model.Peca pecaDomain = new com.oficina.estoque.domain.model.Peca(peca.getId(), peca.getDescricao(), entity.getValorUnitario().doubleValue(), peca.getQuantidade());
         return new PecasNecessarias(pecaDomain, entity.getQuantidade());
     }
 
-    private OrdemDeServicoServicosEntity toServicoEntity(int ordemId, Servico servico) {
-        return new OrdemDeServicoServicosEntity(ordemId, servicoRepository.getReferenceById(servico.getId()),
+    private OrdemDeServicoServicosEntity toServicoEntity(int ordemId, com.oficina.estoque.domain.model.Servico servico, ServicoEntity servicoEntity) {
+        return new OrdemDeServicoServicosEntity(ordemId, servicoEntity,
                 BigDecimal.valueOf(servico.getValor()));
     }
 
-    private Servico toServicoDomain(OrdemDeServicoServicosEntity entity) {
+    private com.oficina.estoque.domain.model.Servico toServicoDomain(OrdemDeServicoServicosEntity entity) {
         ServicoEntity servico = entity.getServico();
-        return new Servico(servico.getId(), servico.getDescricao(), entity.getValorCobrado().doubleValue());
+        return new com.oficina.estoque.domain.model.Servico(servico.getId(), servico.getDescricao(), entity.getValorCobrado().doubleValue());
     }
 }

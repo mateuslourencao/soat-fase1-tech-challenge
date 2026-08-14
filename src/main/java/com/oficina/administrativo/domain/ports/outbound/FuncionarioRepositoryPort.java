33b838cd55e68272134b1c73d77e1dc5db50c8e5
Mateus Lourencao <mateusldias@gmail.com>
2026-08-14 01:51:31 -0300

    feat: incluindo o readme

--- src/main/java/com/oficina/administrativo/domain/ports/outbound/FuncionarioRepositoryPort.java
+++ src/main/java/com/oficina/administrativo/domain/ports/outbound/FuncionarioRepositoryPort.java
@@ -2,8 +2,8 @@ package com.oficina.administrativo.domain.ports.outbound;
 
 import com.oficina.administrativo.domain.model.Funcionario;
 
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 
 public interface FuncionarioRepositoryPort {
     Optional<Funcionario> buscarPorEmail(String email);

48185723e2a32ac227b8d4b64347d4cf440104f6
Mateus Lourencao <mateusldias@gmail.com>
2026-08-15 17:30:41 -0300

    fix: ajuste no contador do loop para nao ter loop infinito

--- src/main/java/com/oficina/common/domain/validation/CpfCnpjValidator.java
+++ src/main/java/com/oficina/common/domain/validation/CpfCnpjValidator.java
@@ -28,8 +28,8 @@ public class CpfCnpjValidator implements ConstraintValidator<CpfCnpj, String> {
 
         int[] weight = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
         
-        int d1 = calculateDigit(cpf.substring(0, 9), weight, 10);
-        int d2 = calculateDigit(cpf.substring(0, 9) + d1, weight, 11);
+        int d1 = calculateDigit(cpf.substring(0, 9), weight);
+        int d2 = calculateDigit(cpf.substring(0, 9) + d1, weight);
         
         return cpf.equals(cpf.substring(0, 9) + d1 + d2);
     }
@@ -40,16 +40,16 @@ public class CpfCnpjValidator implements ConstraintValidator<CpfCnpj, String> {
         int[] weight1 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
         int[] weight2 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
         
-        int d1 = calculateDigit(cnpj.substring(0, 12), weight1, 13);
-        int d2 = calculateDigit(cnpj.substring(0, 12) + d1, weight2, 14);
+        int d1 = calculateDigit(cnpj.substring(0, 12), weight1);
+        int d2 = calculateDigit(cnpj.substring(0, 12) + d1, weight2);
         
         return cnpj.equals(cnpj.substring(0, 12) + d1 + d2);
     }
 
-    private int calculateDigit(String str, int[] weight, int length) {
+    private int calculateDigit(String str, int[] weight) {
         int sum = 0;
-        for (int i = str.length() - 1, digit; i >= 0; i--) {
-            digit = Integer.parseInt(str.substring(i, i + 1));
+        for (int i = str.length() - 1; i >= 0; i--) {
+            int digit = Integer.parseInt(str.substring(i, i + 1));
             sum += digit * weight[weight.length - str.length() + i];
         }
         sum = 11 - sum % 11;
